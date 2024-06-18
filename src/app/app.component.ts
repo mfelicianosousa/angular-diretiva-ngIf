76d539c10160aee66b3e6b48592a06773e143893
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-18 10:31:40 -0400

    Diretiva Strutural ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngif';
+  isView = true;
+  
+  pessoa: any = {
+    status: 1,
+    name: undefined,
+  }
+
+  onToggleProp(){
+    
+    this.isView = !this.isView;
+  }
+
+  isTrusthy(){
+    //return {};	
+     // return [];
+    // return true;
+    // return 1;
+    // return “teste”;
+    // return “0”;
+          // return “false”;
+    return this.pessoa.status;
+  }
+  isFalsy(){
+   // return false;	
+   return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    //return this.pessoa.name ;
+    return this.pessoa.endereco?.rua;
+  }
+  
 }
